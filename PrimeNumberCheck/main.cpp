c229cc32604563ac72cf279ba0e0336f975bb2f0
Bilal Suleman <bilsuleman@gmail.com>
2020-02-29 00:58:00 -0600

    Finished generator, abandoned use of sieve, too complex to implement with remove_if, can be used to accomplish same task differently without remove_if and more efficient.

--- PrimeNumberCheck/main.cpp
+++ PrimeNumberCheck/main.cpp
@@ -1,8 +1,30 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
+
+class isComposite
+{
+public:
+	bool operator()(int x)
+	{
+		// Corner cases 
+		if (x <= 1)  return false;
+		if (x <= 3)  return false;
+
+		// This is checked so that we can skip  
+		// middle five numbers in below loop 
+		if (x % 2 == 0 || x % 3 == 0) return true;
+
+		for (int i = 5; i * i <= x; i = i + 6)
+			if (x % i == 0 || x % (i + 2) == 0)
+				return true;
+
+		return false;
+	}
+};
 
 int main()
 {
@@ -11,12 +33,26 @@ int main()
 	cin >> x;
 
 	vector<int> v = { 2 };
+	// vector<bool> b;
+
+	/*for (int i = 2; i <= x; i++)
+	{
+		b.push_back(true);
+	}*/
 
 	for (int i = 3; i <= x; i++)
 	{
 		v.push_back(i);
 	}
 
+	/*for (int a : v)
+	{
+		cout << a << " ";
+	}
+	cout << endl;*/
+
+	v.erase(remove_if(begin(v), end(v), isComposite()), end(v));
+	cout << "The primes less than " << x << " are as follows: " << endl;
 	for (int a : v)
 	{
 		cout << a << " ";
